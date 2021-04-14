659b98548d2fabbda05475b931946e485ab832fb
Michael Arcidiacono <mixarcidiacono@gmail.com>
2021-04-14 14:09:57 -0700

    uhhh think memory leak is fixed I dunno

---
+++ sync_zinc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+kalki="boris@golab.zapto.org:/home/boris/Data/Zinc"
+bb="jack@bb.zapto.org:/home/jack/Data/Zinc"
+home="$HOME/Data/Zinc"
+
+if [ $1 = "kalki" ]; then
+    echo "grabbing from kalki"
+    #scp $kalki/files.txt $home/
+    scp $kalki/files_filtered_mol.txt $home/
+    #scp $home/files.txt $bb/
+    scp $home/files_filtered_mol.txt $bb/
+elif [ $1 = "bb" ]; then
+    echo "grabbing from bb"
+    scp $bb/files.txt $home/
+    scp $bb/files_filtered*.txt $home/
+    scp $home/files.txt $kalki/
+    scp $home/files_filtered*.txt $kalki/
+else
+    echo "sending to all"
+    scp $home/files.txt $bb/
+    scp $home/files_filtered*.txt $bb/
+    scp $home/files.txt $kalki/
+    scp $home/files_filtered*.txt $kalki/
+fi
