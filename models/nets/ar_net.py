fixed bond off by one

models/nets/ar_net.py
@@ -102,20 +102,20 @@ class ArNetDecoder(nn.Module):
 
         atypes = get_padded_atypes(tmol, device, batch_size, truncate_atoms)
         aenc = self.atom_embed(atypes)
-        aenc = self.atom_enc(aenc, tmol.bonds)
+        aenc = self.atom_enc(aenc, tmol.bonds, True)
 
         valences = get_padded_valences(tmol, device, batch_size, truncate_atoms)
         venc = self.valence_embed(valences, device)
-        venc = self.valence_enc(venc, tmol.bonds)
+        venc = self.valence_enc(venc, tmol.bonds, True)
         
         kpenc = get_padded_kps(tmol, device, batch_size, truncate_atoms)
         kpenc = self.kp_init_enc(kpenc)
-        kpenc = self.kp_enc(kpenc, tmol.bonds)
+        kpenc = self.kp_enc(kpenc, tmol.bonds, True)
         kpenc = kpenc.contiguous().view(kpenc.size(0), kpenc.size(1), -1)
-        kpenc = self.kp_flat_enc(kpenc, tmol.bonds)
+        kpenc = self.kp_flat_enc(kpenc, tmol.bonds, True)
 
         enc = torch.cat((aenc, venc, kpenc), 2)
-        enc = self.final_enc(enc, tmol.bonds)
+        enc = self.final_enc(enc, tmol.bonds, True)
 
         lat_in = self.lat_fc(z).unsqueeze(1).repeat(1, atypes.size(1), 1)
         rnn_in = torch.cat([lat_in, enc], 2)

models/nets/attention_utils.py
@@ -7,9 +7,9 @@ from .bond_attention import *
 
 class AtnDownConv(nn.Module):
 
-    def __init__(self, in_f, out_f, one_way):
+    def __init__(self, in_f, out_f, *args):
         super().__init__()
-        self.atn = BondAttentionFixed(one_way)
+        self.atn = BondAttentionFixed(*args)
         self.conv = TimeDistributed(
             nn.Sequential(
                 nn.Conv3d(in_f*2, out_f, kernel_size=3, bias=False, padding=1),
@@ -20,15 +20,15 @@ class AtnDownConv(nn.Module):
             axis=2
         )
 
-    def forward(self, x, bonds):
-        x = self.atn(x, bonds)
+    def forward(self, x, *args):
+        x = self.atn(x, *args)
         return self.conv(x)
 
 class AtnUpConv(nn.Module):
 
-    def __init__(self, in_f, out_f, one_way):
+    def __init__(self, in_f, out_f, *args):
         super().__init__()
-        self.atn = BondAttentionFixed(one_way)
+        self.atn = BondAttentionFixed(*args)
         self.conv = TimeDistributed(
             nn.Sequential(
                 upsample(in_f*2, out_f),
@@ -39,8 +39,8 @@ class AtnUpConv(nn.Module):
             axis=2
         )
 
-    def forward(self, x, bonds):
-        x = self.atn(x, bonds)
+    def forward(self, x, *args):
+        x = self.atn(x, *args)
         return self.conv(x)
 
 class AtnFlat(nn.Module):

models/nets/bond_attention.py
@@ -35,9 +35,13 @@ class BondAttentionFixed(nn.Module):
         super().__init__()
         self.one_way = one_way
 
-    def forward(self, x, bonds):
+    def forward(self, x, bonds, is_padded=False):
         out = torch.zeros_like(x)
         for batch, start, end, bond in bonds.get_all_indexes():
+            if is_padded:
+                start += 1
+                end += 1
+            if end >= bonds.data.size(2): continue
             out[batch, end] += x[batch, start]
             if not self.one_way:
                 out[batch, start] += x[batch, end]